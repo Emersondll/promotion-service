33a17509d3f93aa8ece6f3d16eab536e90fff908
Emerson Lima <emersondll@outlook.com>
2026-03-17 17:51:11 -0300

    Commit automático do arquivo api/src/main/java/com/abinbev/b2b/promotion/constants/NewRelicParameters.java

---
+++ api/src/main/java/com/abinbev/b2b/promotion/constants/NewRelicParameters.java
@@ -0,0 +1,18 @@
+package com.abinbev.b2b.promotion.constants;
+
+public final class NewRelicParameters {
+
+  private NewRelicParameters() {}
+
+  public static class REQUEST {
+    public static final String args = "requestArgs";
+  }
+
+  public static class RESPONSE {
+    public static final String statusCode = "statusCode";
+  }
+
+  public static final String PAGE = "page";
+  public static final String PAGE_SIZE = "pageSize";
+
+}
