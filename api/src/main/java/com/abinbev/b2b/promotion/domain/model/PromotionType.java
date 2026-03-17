2925b5030b3ce43189b8fbbe39bda3f371221be8
Emerson Lima <emersondll@outlook.com>
2026-03-17 17:51:40 -0300

    Commit automático do arquivo api/src/main/java/com/abinbev/b2b/promotion/domain/model/PromotionType.java

---
+++ api/src/main/java/com/abinbev/b2b/promotion/domain/model/PromotionType.java
@@ -0,0 +1,10 @@
+package com.abinbev.b2b.promotion.domain.model;
+
+public enum PromotionType {
+  STEPPED_DISCOUNT,
+  STEPPED_FREE_GOOD,
+  DISCOUNT,
+  FREE_GOOD,
+  FLEXIBLE_DISCOUNT,
+  FIXED_COMBO;
+}
