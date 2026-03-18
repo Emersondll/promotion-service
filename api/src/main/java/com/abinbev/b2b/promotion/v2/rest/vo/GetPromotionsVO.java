07d4ce87ec703c4005d6a81a88e978ee501d08bf
Emerson Lima <emersondll@outlook.com>
2026-03-18 09:30:55 -0300

    Commit automático do arquivo api/src/main/java/com/abinbev/b2b/promotion/v2/rest/vo/GetPromotionsVO.java

---
+++ api/src/main/java/com/abinbev/b2b/promotion/v2/rest/vo/GetPromotionsVO.java
@@ -0,0 +1,24 @@
+package com.abinbev.b2b.promotion.v2.rest.vo;
+
+import com.abinbev.b2b.promotion.domain.model.Pagination;
+import java.util.List;
+
+public class GetPromotionsVO {
+
+  private final List<PromotionResponse> promotions;
+  private final Pagination pagination;
+
+  public GetPromotionsVO(final List<PromotionResponse> promotions, final Pagination pagination) {
+
+    this.promotions = promotions;
+    this.pagination = pagination;
+  }
+
+  public List<PromotionResponse> getPromotions() {
+    return promotions;
+  }
+
+  public Pagination getPagination() {
+    return pagination;
+  }
+}
