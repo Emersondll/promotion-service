948e2f24f7df8ce2a6d988f7dc8b57e4011a157a
Emerson Lima <emersondll@outlook.com>
2026-03-18 09:31:47 -0300

    Commit automático do arquivo api/src/main/java/com/abinbev/b2b/promotion/validators/annotations/NotDuplicate.java

---
+++ api/src/main/java/com/abinbev/b2b/promotion/validators/annotations/NotDuplicate.java
@@ -0,0 +1,22 @@
+package com.abinbev.b2b.promotion.validators.annotations;
+
+import com.abinbev.b2b.promotion.validators.NotDuplicateValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = NotDuplicateValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotDuplicate {
+  String message() default "there-should-be-no-duplicated-values";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
