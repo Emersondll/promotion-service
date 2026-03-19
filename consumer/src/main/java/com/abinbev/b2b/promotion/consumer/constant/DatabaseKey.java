05ca6a5ca089b869d6971582ae180e9597bedfeb
Emerson Lima <emersondll@outlook.com>
2026-03-19 17:59:04 -0300

    Commit automático do arquivo consumer/src/main/java/com/abinbev/b2b/promotion/consumer/constant/DatabaseKey.java

---
+++ consumer/src/main/java/com/abinbev/b2b/promotion/consumer/constant/DatabaseKey.java
@@ -0,0 +1,36 @@
+package com.abinbev.b2b.promotion.consumer.constant;
+
+public final class DatabaseKey {
+
+  public static final String ID = "id";
+  public static final String ACCOUNT_ID = "accountId";
+  public static final String PROMOTION_ID = "promotionId";
+  public static final String INTERNAL_ID = "internalId";
+  public static final String IMAGE = "image";
+  public static final String DELETED = "deleted";
+  public static final String DISABLED = "disabled";
+  public static final String UPDATE_AT = "updateAt";
+  public static final String SKUS = "skus";
+  public static final String FREE_GOOD_SKUS = "freeGoodSkus";
+  public static final String VENDOR_ITEM_IDS = "vendorItemIds";
+  public static final String FREE_GOOD_VENDOR_ITEM_IDS = "freeGoodVendorItemIds";
+  public static final String START_DATE = "startDate";
+  public static final String END_DATE = "endDate";
+  public static final String PROMOTION_TYPE = "promotionType";
+  public static final String TITLE = "title";
+  public static final String DESCRIPTION = "description";
+  public static final String BUDGET = "budget";
+  public static final String QUANTITY_LIMIT = "quantityLimit";
+  public static final String CREATED_AT = "createdAt";
+  public static final String VENDOR_PROMOTION_ID = "vendorPromotionId";
+  public static final String VENDOR_ID = "vendorId";
+  public static final String VENDOR_ACCOUNT_ID = "vendorAccountId";
+  public static final String PROMOTION_PLATFORM_ID = "promotionPlatformId";
+  public static final String DELETED_AT = "deletedAt";
+  public static final String TRANSLATIONS = "translations";
+  public static final String DEFAULT_LANGUAGE = "defaultLanguage";
+  public static final String HIGHEST_DEAL_END_DATE = "highestDealEndDate";
+  public static final String LOWEST_DEAL_END_DATE = "lowestDealStartDate";
+
+  private DatabaseKey() {}
+}
