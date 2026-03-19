e5d1085ad820a762366c1899e926179bebfb95b8
Emerson Lima <emersondll@outlook.com>
2026-03-19 17:59:58 -0300

    Commit automático do arquivo consumer/src/main/java/com/abinbev/b2b/promotion/consumer/helper/OffsetDateTimeSerializer.java

---
+++ consumer/src/main/java/com/abinbev/b2b/promotion/consumer/helper/OffsetDateTimeSerializer.java
@@ -0,0 +1,27 @@
+package com.abinbev.b2b.promotion.consumer.helper;
+
+import static com.abinbev.b2b.promotion.consumer.constant.ApiConstants.DATE_PATTERN;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class OffsetDateTimeSerializer extends JsonSerializer<OffsetDateTime> {
+
+  @Override
+  public void serialize(
+      final OffsetDateTime offsetDateTime,
+      final JsonGenerator jsonGenerator,
+      final SerializerProvider serializerProvider)
+      throws IOException {
+    if (offsetDateTime != null) {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+      jsonGenerator.writeString(formatter.format(offsetDateTime));
+    } else {
+      jsonGenerator.writeNull();
+    }
+  }
+}
